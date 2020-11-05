a920eb7bfc0753a87d9ca061bd162279c3c53aa3
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-11-04 23:55:21 -0800

    Created Student Controller

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-    has_secure_password # allowed to use authentication
+    has_secure_password # allowed to use authentication for 
 has_many :courses
 has_many :reviews
 has_many :reviewed_courses, through: :reviews, :sourse :course
