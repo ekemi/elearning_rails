158c7ac1ae92010c4bf46c26a61071c4443198d5
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-11-04 23:17:08 -0800

    Added has_secure_password and gem bcrypt

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   belongs_to :student
+  has_many :reviews
+  has_many :students, through: :reviews
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+    has_secure_password # allowed to use authentication
+has_many :courses
+has_many :reviews
+has_many :reviewed_courses, through: :reviews, :sourse :course
+
+
+
 end
